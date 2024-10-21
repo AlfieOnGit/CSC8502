c18396ef8ae4aa016e153b22f96b2e2ac7b86d20
Alfie <alfiejay@mail.com>
2024-10-21 15:17:15 +0100

    Initial commit

---
+++ 1) Your First Triangle/Renderer.cpp
@@ -0,0 +1,41 @@
+﻿#include "Renderer.h"
+
+Renderer::Renderer(Window& parent): OGLRenderer(parent)
+{
+    triangles[0] = Mesh::GenerateTriangle(
+        Vector3(0.5f, 0.5f, 0.0f),
+        Vector3(0.5f, -0.5f, 0.0f),
+        Vector3(-0.5f, -0.5f, 0.0f));
+    triangles[1] = Mesh::GenerateTriangle(
+        Vector3(0.5f, 0.5f, 0.0f),
+        Vector3(-0.5f, 0.5f, 0.0f),
+        Vector3(-0.5f, -0.5f, 0.0f));
+
+    basicShader = new Shader("basicVertex.glsl", "colourFragment.glsl");
+
+    if (!basicShader->LoadSuccess())
+    {
+        return;
+    }
+    init = true;
+}
+
+Renderer::~Renderer()
+{
+    delete[] triangles;
+    delete basicShader;
+}
+
+void Renderer::RenderScene()
+{
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    BindShader(basicShader);
+    for (Mesh *t : triangles)
+    {
+        t->Draw();
+    }
+}
+
+
---
+++ 1) Your First Triangle/Renderer.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "NCLGL/OGLRenderer.h"
+
+class Renderer : public OGLRenderer
+{
+public:
+    Renderer(Window &parent);
+    virtual ~Renderer();
+    virtual void RenderScene();
+protected:
+    Mesh *triangles[2];
+    Shader* basicShader;
+};
