1acbe37379caa5850b47f4eb346af60b61d9f83f
Alfie <alfiejay@mail.com>
2024-11-13 06:07:16 +0000

    Tutorial 10

---
+++ 10) Post Processing/Renderer.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "nclgl/Camera.h"
+#include "nclgl/HeightMap.h"
+#include "nclgl/OGLRenderer.h"
+
+class Renderer : public OGLRenderer
+{
+public:
+    Renderer(Window &parent);
+    ~Renderer();
+
+    void RenderScene() override;
+    void UpdateScene(float dt) override;
+
+protected:
+    void PresentScene();
+    void DrawPostProcess();
+    void DrawScene();
+
+    Shader *sceneShader;
+    Shader *processShader;
+
+    Camera *camera;
+    Mesh *quad;
+    HeightMap *heightMap;
+    GLuint heightTexture;
+    GLuint bufferFBO;
+    GLuint processFBO;
+    GLuint bufferColourTex[2];
+    GLuint bufferDepthTex;
+};
