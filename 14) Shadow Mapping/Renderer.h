f6c0ddb0cd2b8176b826755126b6fb4a55db200e
Alfie <alfiejay@mail.com>
2024-11-14 06:11:16 +0000

    Tutorial 14

---
+++ 14) Shadow Mapping/Renderer.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "nclgl/OGLRenderer.h"
+
+class Camera;
+class Mesh;
+
+class Renderer : public OGLRenderer
+{
+public:
+    Renderer(Window &parent);
+    ~Renderer();
+
+    void UpdateScene(float dt) override;
+    void RenderScene() override;
+    
+protected:
+    void DrawShadowScene();
+    void DrawMainScene();
+
+    GLuint shadowTex, shadowFBO;
+
+    GLuint sceneDiffuse, sceneBump;
+    float sceneTime;
+
+    Shader *sceneShader, *shadowShader;
+
+    vector<Mesh*> sceneMeshes;
+    vector<Matrix4> sceneTransforms;
+
+    Camera *camera;
+    Light *light;
+};
---
+++ Shaders/shadowFrag.glsl
@@ -0,0 +1,7 @@
+﻿#version 330 core
+
+out vec4 fragColour;
+
+void main() {
+    fragColour = vec4(1.0);
+}
