d98620bc5769451cb75ac1b8871379d117891675
Alfie <alfiejay@mail.com>
2024-11-14 04:53:23 +0000

    Tutorial 13

---
+++ 13) Cube Mapping/Renderer.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "nclgl/OGLRenderer.h"
+
+class HeightMap;
+class Camera;
+
+class Renderer : public OGLRenderer
+{
+public:
+    Renderer(Window &parent);
+    ~Renderer();
+    void RenderScene() override;
+    void UpdateScene(float dt) override;
+protected:
+    void DrawHeightMap();
+    void DrawWater();
+    void DrawSkybox();
+
+    Shader *lightShader, *reflectShader, *skyboxShader;
+    HeightMap *heightMap;
+    Mesh *quad;
+
+    Light *light;
+    Camera *camera;
+
+    GLuint cubeMap, waterTex, earthTex, earthBump;
+
+    float waterRotate, waterCycle;
+};
